44e1957cd0adb9815e30246265edd8e6479543f0
3lNoeDBaston <dhernandezr2214@alumno.ipn.mx>
2024-10-24 16:18:40 -0600

    b

--- Clase 24102024/scrript.js
+++ Clase 24102024/scrript.js
@@ -1,1 +1,28 @@
-alert("makeine esta god")
+//arreglos
+
+let array = ["dato1", 1, Boolean];
+//pushs es meterle algun dato
+console.log("Tamaño del arreglo: "+ array.length);
+array.push("5");
+console.log(array[3])
+console.log("Tamaño dle nuevo array: "+array.length);
+
+/*
+5*1 = 5
+5*2 = 10
+5*3= 15
+5*4= 20
+5*5 = 25
+5*6= 30
+fin del arreglo posicion= 30
+*/
+
+let a=10;
+let x=5
+let tablita= [];
+for(let m=1; m<=a; m++){
+    tablita[m]= m*x;
+}
+
+console.log("Fin del arreglo con tamaño: "+ tablita.length+ " y valor en la posicion: "+ tablita[6])
+
